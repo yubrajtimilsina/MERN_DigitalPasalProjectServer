ce314e0c8da7b9aa24492ed89a33bcd88204df72
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2024-10-17 10:20:05 +0545

    commit

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,6 @@ import {config} from 'dotenv'
 config()
 
 export const envConfig ={
-    port : process.env.PORT
-    connectionString : process.env.CONNECTION_STRING
+    port : process.env.PORT,
+    connectionString : process.env.CONNECTION_STRING,
 }
