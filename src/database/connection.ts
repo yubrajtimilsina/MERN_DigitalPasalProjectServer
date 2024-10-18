e77bc66ab7704b615e44bdf6abfe78476df74cdd
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2024-10-18 21:23:45 +0545

    commit

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
 import { envConfig } from "../config/config";
 
-const sequelize = new Sequelize(envConfig.connectionString as string)
+const sequelize = new Sequelize(envConfig.connectionString as string,{
+    models : [__dirname + '/models']
+})
 
 try {
     sequelize.authenticate()
     .then(()=>{
-        console.log("milyo haiii authentication!!!")
+        console.log("milyo haiii authentication chaii!!!")
     })
     .catch(error=>{
         console.log("error aayo", error)
@@ -15,5 +17,9 @@ try {
     console.log(error)
 
 }
+
+sequelize.sync({force : false}).then(()=>{
+    console.log("local changes injected to database successfully")
+})
 
 export default sequelize
