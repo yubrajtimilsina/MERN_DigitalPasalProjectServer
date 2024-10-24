d8053b345a40af2ca54d6618da6f5037a580401e
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2024-10-24 12:05:21 +0545

    commit

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -27,7 +27,7 @@ class UserController{
     }
     static async login(req:Request,res:Response){
         // accept incoming data --> email, password
-        const {email, password} = req.body // password - manish --> hash() --> $234234324fjlsdf
+        const {email, password} = req.body // password - yubraj --> hash() --> $234234324fjlsdf
         if(!email || !password){
             res.status(400).json({
                 message : "Please provide email, password"
@@ -69,4 +69,3 @@ class UserController{
 
 
 export default UserController
-
